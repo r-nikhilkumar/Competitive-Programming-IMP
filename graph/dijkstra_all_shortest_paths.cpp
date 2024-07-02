186d44f974ed6f4dd7629e8e5f46e9c2e2e57404
Nikhil Kumar <thenikhilkumar1@gmail.com>
2024-07-02 20:03:35 +0530

    dijkstra_all_shortest_path

---
+++ graph/dijkstra_all_shortest_paths.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPaths(vector<vector<int>> &parent, int v, int &numPath);
+
+void solve() {
+    int v, e;
+    cin >> v >> e;
+
+    vector<vector<pair<int, int>>> adjL(v);
+    for (int i = 0; i < e; ++i) {
+        int s, d, w;
+        cin >> s >> d >> w;
+        adjL[s].push_back({d, w});
+    }
+    int s, d;
+    cin >> s >> d;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, s});
+    vector<int> dist(v, 1e9);
+    dist[s] = 0;
+    vector<vector<int>> parent(v);
+
+    for (int i = 0; i < parent.size(); ++i) {
+        parent[i].push_back(i);
+    }
+
+    while (!pq.empty()) {
+        auto it = pq.top();
+        pq.pop();
+        int it_s = it.second;
+        int it_w = it.first;
+        if (it_s == d) break;
+        for (auto i : adjL[it_s]) {
+            if (dist[i.first] > i.second + it_w) {
+                parent[i.first].clear();
+                parent[i.first].push_back(it_s);
+                dist[i.first] = i.second + it_w;
+                pq.push({dist[i.first], i.first});
+            } else if (dist[i.first] == i.second + it_w) {
+                parent[i.first].push_back(it_s);
+            }
+        }
+    }
+
+    if (dist[d] != 1e9) {
+        int numPath = 0;
+        printPaths(parent, d, numPath);
+        cout << numPath << endl;
+    } else {
+        cout << -1 << endl;
+    }
+}
+
+void printPaths(vector<vector<int>> &parent, int v, int &numPath) {
+    if (parent[v].size() == 1 && parent[v][0] == v) {
+        // cout << v << " ";
+        numPath++;
+        return;
+    }
+    for (auto i : parent[v]) {
+        printPaths(parent, i, numPath);
+        // cout << v << " ";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
